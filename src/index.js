ebcf85b111043b813d4b25ad173230b7c23b7c13
Jorge Dias <jorgediasdsg@gmail.com>
2020-09-26 00:44:39 -0300

    🚀 feat: add user in and user out

--- src/index.js
+++ src/index.js
@@ -1,32 +1,49 @@
 const request = require('request');
 const express = require('express');
-const axios = require('axios')
+const { Curl } = require('node-libcurl');
 require('dotenv/config');
-
+ 
 const app = express();
 
 app.use(express.json());
 
 const users = [];
-const callQueue = [];
+var callQueue = [];
 var nextId = "";
 var newNext = "";
 var record = 0;
+var oldQueue = [];
+
+
+
 
 
 function sendDiscord(message){
-axios
-  .post(DISCORD_URL_BOT, {
-    todo: message
-  })
-  .then(res => {
-    console.log(`statusCode: ${res.statusCode}`)
-    console.log(res)
-  })
-  .catch(error => {
-    console.error(error)
-  })
+    const urlRocket="https://flin.rocket.chat/hooks/Nf7dtuaikgsi64aCa/ayLSatokYP2Z3gpYAWEDsg5zp7iDPxPPY6AkEfwJkRHgiCqb"
+    const urlAzkozia="http://187.49.226.34:23600/cfe/wallboard/wallboard.php?queue=CALLFLOW-29969517258331e2684e1b-QUEUE-60&size=medium&show_agents&colorize"
+    // const msgJson="{\"alias\":\"Azkozia Bot\",\"text\": \"${message}\",\"attachments\":[{\"title\":\"AZKOZIA\",\"title_link\": \"${urlAzkozia}\",\"text\":\"Call\",\"color\":\"#FFAAAA\"}]}";
+    // curl -H "Content-Type: application/json" -X POST -d "{\"alias\":\"Nagios Bot\",\"text\": \"$MSG\",\"attachments\":[{\"title\":\"NAGIOS\",\"title_link\": \"$LINK\",\"text\":\"$HEADER\",\"color\":\"#FFAAAA\"}]}" $url_rocket
+
+    const curl = new Curl();
+ 
+    curl.setOpt('URL', urlRocket);
+    curl.setOpt('FOLLOWLOCATION', true);
+    
+    curl.on('end', function (statusCode, data, headers) {
+    console.info(statusCode);
+    console.info('---');
+    console.info(data.length);
+    console.info('---');
+    console.info(this.getInfo( 'TOTAL_TIME'));
+    
+    this.close();
+});
+
+curl.on('error', curl.close.bind(curl));
+curl.perform();
+
 }
+sendDiscord("Sistema de monitoramento iniciado!");
 
 function loadUsers(){
     //load callQueue
@@ -36,12 +53,12 @@ function loadUsers(){
             all.forEach(function (extension, array) {
                 const userId = extension.extension;
                 const userIndex = users.findIndex(user => user.userId == userId);
-
+                
                 const user = {
                     userId: extension.extension,
                     callerid: extension.callerid,
                     descr: extension.descr,
-                  };
+                };
                 if (userIndex < 0){
                     console.log("add "+user.userId+" Name:"+user.callerid)
                     users.push(user);
@@ -63,8 +80,13 @@ function server(){
     request(process.env.ASKOZIA_QUEUE_SHOW_STATUS, function (error, response, body) {  
         if (!error && response.statusCode == 200) {
             const { agents } = JSON.parse(body);
-            // console.table(agents);
-
+            console.table(agents);
+            if (!agents){
+                console.log("Askozia offline")
+                return
+            } else {
+                console.log("Askozia Online")
+            }
             agents.forEach(function (extension, array) {
                 const userId = extension.extension;
                 const ramal = "20"+extension.extension
@@ -89,13 +111,20 @@ function server(){
                 const callQueueIndex = callQueue.findIndex(user => user.userId == userId);
                 if (callQueueIndex < 0){
                     callQueue.push(user);
+                    console.log(`"Usuário ${user.name} acabou de logar!`)
                 } else {
                     callQueue[callQueueIndex] = user;
                 }
             });
-
         }
     });
+    oldQueue.forEach(function (user, array) {
+        const userIndex = callQueue.findIndex(user => user.userId == userId);
+        if (userIndex < 0){
+            console.log(`Usuário ${user.name} acabou de sair!`)
+        }
+    });
+    
     callQueue.forEach(function (user, array) {
         if(user.lastCall == '00:00:00'){
             user.lastCall='12:00:00';
@@ -123,11 +152,14 @@ function server(){
         if (agentIndex < 0){
             console.log("User not found!")
         } else {
-            //message = users[agentIndex].callerid + " are the next!";
-            //sendDiscord(message);
+            message = users[agentIndex].callerid + " are the next!";
+            sendDiscord(message);
         }
     }
     console.table(callQueue);
+
+    oldQueue = callQueue;
+    callQueue = [];
 }
 
 setInterval(server, 1000);
